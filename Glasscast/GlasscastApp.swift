ac96b5d57e6fde2a3df3a3dde5e0fddf0e47e081
Aryansingh0810 <ss0854850@gmail.com>
2026-01-20 00:29:07 +0530

    Initial Commit

---
+++ Glasscast/GlasscastApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlasscastApp.swift
+//  Glasscast
+//
+//  Created by Aryan Singh on 20/01/26.
+//
+
+import SwiftUI
+
+@main
+struct GlasscastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
